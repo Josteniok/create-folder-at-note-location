2c354fd01fcc574223bc25d3c5aa46802b789501
Justin Ennis <justin@nrc-llc.com>
2026-04-24 09:11:27 -0700

    Got rid of the setting and instead shifted to just using two different commands - one that adds a folder and a file and one that adds just the folder.

--- src/main.ts
+++ src/main.ts
@@ -1,83 +1,85 @@
 import { App, Modal, Setting, Notice, Plugin } from "obsidian";
-import {
-	DEFAULT_SETTINGS,
-	CreateFolderAtNoteLocationSettings,
-	FolderAtNoteLocationPluginSettingsTab,
-} from "./settings";
 
 export default class CreateFolderAtNoteLocation extends Plugin {
-	settings: CreateFolderAtNoteLocationSettings;
-
 	async onload() {
-		await this.loadSettings();
-
 		// This adds a folder where the currently open note is located
 		this.addCommand({
 			id: "add-folder-where-open-note-is",
 			name: "Add folder here",
 			callback: () => {
-				this.addFolder();
+				const filePath = this.app.workspace.getActiveFile()?.path;
+
+				if (filePath) {
+					new GetFolderName(this.app, (result) => {
+						const folderPath =
+							filePath.substring(0, filePath.lastIndexOf("/")) +
+							"/" +
+							result;
+						this.app.vault.createFolder(folderPath).then(
+							() => {},
+							(error) => {
+								new Notice(
+									"Folder not created. Folder already exists.",
+								);
+							},
+						);
+					}).open();
+				} else {
+					new Notice("Folder not created. No active note found.");
+				}
 			},
 		});
 
-		this.addSettingTab(
-			new FolderAtNoteLocationPluginSettingsTab(this.app, this),
-		);
+		// This adds a folder where the currently open note is located
+		// and also a note in the folder with the same name as the folder,
+		// which is useful for the Waypoints plugin.
+		this.addCommand({
+			id: "add-folder-with-note-where-open-note-is",
+			name: "Add folder with note here",
+			callback: () => {
+				const filePath = this.app.workspace.getActiveFile()?.path;
+
+				if (filePath) {
+					new GetFolderName(this.app, (result) => {
+						const folderPath =
+							filePath.substring(0, filePath.lastIndexOf("/")) +
+							"/" +
+							result;
+						this.app.vault.createFolder(folderPath).then(
+							(value) => {
+								this.app.vault
+									.create(
+										folderPath + "/" + result + ".md",
+										"",
+									)
+									.then(
+										(value) => {
+											void this.app.workspace
+												.getLeaf(true)
+												.openFile(value);
+										},
+										(error) => {
+											new Notice(
+												"File not created. File already exists.",
+											);
+										},
+									);
+							},
+							(error) => {
+								new Notice(
+									"Folder not created. Folder already exists.",
+								);
+							},
+						);
+					}).open();
+				} else {
+					new Notice("Folder not created. No active note found.");
+				}
+			},
+		});
 	}
 
 	onunload() {}
-
-	async loadSettings() {
-		this.settings = Object.assign(
-			{},
-			DEFAULT_SETTINGS,
-			(await this.loadData()) as Partial<CreateFolderAtNoteLocationSettings>,
-		);
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-
-	addFolder() {
-		const filePath = this.app.workspace.getActiveFile()?.path;
-
-		if (filePath) {
-			new GetFolderName(this.app, (result) => {
-				const folderPath =
-					filePath.substring(0, filePath.lastIndexOf("/")) +
-					"/" +
-					result;
-				this.app.vault.createFolder(folderPath).then(
-					(value) => {
-						if (this.settings.createFileInFolderSetting) {
-							this.app.vault
-								.create(folderPath + "/" + result + ".md", "")
-								.then(
-									(value) => {
-										void this.app.workspace
-											.getLeaf(true)
-											.openFile(value);
-									},
-									(error) => {
-										new Notice(
-											"File not created. File already exists.",
-										);
-									},
-								);
-						}
-					},
-					(error) => {
-						new Notice(
-							"Folder not created. Folder already exists.",
-						);
-					},
-				);
-			}).open();
-		} else {
-			new Notice("Folder not created. No active note found.");
-		}
-	}
 }
 
 class GetFolderName extends Modal {
