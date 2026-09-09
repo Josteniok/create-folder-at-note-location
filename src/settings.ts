c1351d85a65ad7f90acebdbda3151d68cca6065b
Justin Ennis <justin@nrc-llc.com>
2026-09-08 18:53:22 -0700

    Version 1.4.0 - Changed to use the new declarative settings and set the minimum app version to 1.13 because of this.

--- src/settings.ts
+++ src/settings.ts
@@ -1,6 +1,5 @@
-import { App, PluginSettingTab, Setting } from "obsidian";
+import { type SettingDefinitionItem, App, PluginSettingTab  } from "obsidian";
 import CreateFolderAtNoteLocation from "./main";
-import { FileSuggest } from "./suggesters/FileSuggester";
 
 export interface CreateFolderAtNoteLocationSettings {
 	folderFileTemplatePath: string;
@@ -18,22 +17,17 @@ export class CreateFolderAtNoteLocationSettingsTab extends PluginSettingTab {
 		this.plugin = plugin;
 	}
 
-	display(): void {
-		const { containerEl } = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName("Folder file template")
-			.setDesc("Template file to use in newly created folders")
-			.addSearch((cb) => {
-				new FileSuggest(this.app, cb.inputEl);
-				cb.setPlaceholder("Enter filename")
-					.onChange(async (value) => {
-						this.plugin.settings.folderFileTemplatePath = value;
-						await this.plugin.saveSettings();
-					})
-					.setValue(this.plugin.settings.folderFileTemplatePath);
-			});
+	getSettingDefinitions(): SettingDefinitionItem[] {
+		return [
+			{
+				name: 'Folder file template',
+				desc: 'Template file to use in newly created folders',
+				control: {
+					type: 'file',
+					key: 'folderFileTemplatePath',
+					placeholder: 'Enter filename'
+				}
+			}
+		]
 	}
 }
